e465e5e6d4a0ba57a6c8a8adaa72b160fff31c55
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-05-13 15:43:00 +0530

    update the backend code

--- redis-client.js
+++ redis-client.js
@@ -1,20 +1,3 @@
-// const Redis = require('ioredis');
-
-// const redis = new Redis({
-//   host: process.env.REDIS_HOST || 'localhost',
-//   port: process.env.REDIS_PORT || 6379,
-//   password: process.env.REDIS_PASSWORD || undefined,
-// });
-
-// redis.on('connect', () => {
-//   console.log('Connected to Redis');
-// });
-
-// redis.on('error', (err) => {
-//   console.error('Redis connection error:', err);
-// });
-
-// module.exports = redis;
 const Redis = require('ioredis');
 
 const redis = new Redis({
@@ -35,4 +18,3 @@ redis.on('error', (err) => {
 });
 
 module.exports = redis;
-
